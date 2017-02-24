a8bccad649b3401b977d962d7c7a95c092b96a90
sostk <poudelsostk@gmail.com>
2017-02-24 13:05:34 +1100

    changes for N as said by ishantvivek

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -172,6 +172,7 @@ struct ipv6_devconf {
 	__s32		disable_ipv6;
 	__s32		accept_dad;
 	__s32		force_tllao;
+	__s32		use_oif_addrs_only;
 	void		*sysctl;
 };
 
@@ -213,6 +214,7 @@ enum {
 	DEVCONF_DISABLE_IPV6,
 	DEVCONF_ACCEPT_DAD,
 	DEVCONF_FORCE_TLLAO,
+        DEVCONF_USE_OIF_ADDRS_ONLY,
 	DEVCONF_MAX
 };
 
@@ -374,7 +376,7 @@ struct ipv6_pinfo {
 	struct ipv6_ac_socklist	*ipv6_ac_list;
 	struct ipv6_fl_socklist *ipv6_fl_list;
 
-	struct ipv6_txoptions	*opt;
+	struct ipv6_txoptions __rcu	*opt;
 	struct sk_buff		*pktoptions;
 	struct sk_buff		*rxpmtu;
 	struct {
